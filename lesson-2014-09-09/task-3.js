16fa324e9ff026422bf16ec8563f55da9dc143c3
Onni Aaltonen <kualtonen@gmail.com>
2014-10-07 18:41:18 +0300

    Lesson 09 09 task files

---
+++ lesson-2014-09-09/task-3.js
@@ -0,0 +1,20 @@
+// task-3.js
+var m = require('./last-mod');
+
+var filename = process.argv[2];
+var timestamp = m(filename);
+if (timestamp !== false) {
+    var date = timestamp;
+
+    var fin = date.getDate() + '.' +
+        (date.getMonth() + 1) + '.' +
+        date.getFullYear();
+
+    fin += '  ' + date.getHours() + '.' +
+        date.getMinutes();
+
+    console.log(fin);
+}
+else {
+    console.log("Error message");
+}
